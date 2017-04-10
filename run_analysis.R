0449a2e24bf3b1bd8aec7c8de383835b66004d9f
s-farr <meetsuziefarrell@gmail.com>
2017-04-09 19:28:50 -0700

    updated script

--- run_analysis.R
+++ run_analysis.R
@@ -4,15 +4,15 @@
 # 2. Extracts only the measurements on the mean and standard deviation for each measurement.
 # 3. Uses descriptive activity names to name the activities in the data set
 # 4. Appropriately labels the data set with descriptive variable names.
-# 5. From the data set in step 4, creates a second, 
-#    independent tidy data set with the average of each variable for each activity and each subject.
+# 5. From the data set in step 4, creates a second, independent tidy data set 
+#    with the average of each variable for each activity and each subject.
 
 library(plyr)
 library(dplyr)
 library(data.table)
 
 # Directory locations
-dataLocation <- '/Users/non-work/datascience_Course3_FinalProj/UCI HAR Dataset/'
+dataLocation <- './UCI HAR Dataset/'
 trainingData <-paste0(dataLocation,'train/')
 testingData <- paste0(dataLocation, 'test/')
 
@@ -72,3 +72,29 @@ levels(allData$activityType) <- activityLabels
 
 # Update Variable Names
 colnames(allData) <- c("SubjectID","Activity","Mean","St.Dev.")
+
+
+# Subject/Activity Summary Table
+dataSummary <- data.frame(matrix(vector(), 30, 13),stringsAsFactors=F)
+x <- paste0(activityLabels, "_mean")
+y <- paste0(activityLabels, "_std")
+z <- sort(c(x,y))
+colnames(dataSummary) <- c('Subject',z)
+
+
+for (i in seq(1:30)) {
+  idx = 2
+  dataSummary[i,] <- i
+  for (j in seq(1,6)){
+    tmp <- filter(allData, SubjectID == i & Activity == activityLabels[j])
+    dataSummary[i,idx] <- mean(tmp$Mean)
+    dataSummary[i,idx+1] <- mean(tmp$St.Dev.)
+    idx = idx+2
+  }
+}
+
+
+# Write out table
+write.table(dataSummary, 'dataSummary.csv',sep = ",", row.names = FALSE)
+
+
